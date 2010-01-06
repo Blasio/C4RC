78f7d575e95b7792a635295d43525c640ca7bbcf
Blasio Muscat <blasio.muscat@gmail.com>
2010-01-06 22:03:05 +0100

    Added testing code in C4RC.cpp to test Events
    Just testing basic use of the class.  This will be extended as the class
    fleshes out.  Should probably pull all of this into a dedicated test
    sub-project so that the classes can be re-tested in case of future
    changes.

--- Server/Source/C4RC.cpp
+++ Server/Source/C4RC.cpp
@@ -6,18 +6,34 @@
 
 using namespace LogWatching;
 
+
 int _tmain(int argc, _TCHAR* argv[])
 {
-	LogWatcher m_LogWatcher(L"C:\\Program Files (x86)\\Activision\\Call of Duty 4 - Modern Warfare\\Mods\\ModWarfare\\games_mp.log");
+	string LogEntry1(" 99:34 J;0123456789abcdef0123456789abcdef;0;Player Name Here");
+	string LogEntry2("199:23 J;0123456789abcdef0123456789abcdef;1;Unknown Soldier");
+	string LogEntry3("2689:04 J;0123456789abcdef0123456789abcdef;2;A Noob");
 
-	Sleep(1000);
+	Event * Ev1 = new Event(LogEntry1);
+	Event * Ev2 = new Event(LogEntry2);
+	Event * Ev3 = new Event(LogEntry3);
 
-	for (int i=0; i<100; i++)
-	{
-		if (m_LogWatcher.Update() > 0)
-			wcout << L"More Bytes" << endl;
-		Sleep(100);
-	}
+	cout << "Event Type: " << Ev1->get_EventType() << ", Client ID: " << Ev1->get_ClientID();
+	cout << ", Name: " << Ev1->get_PlayerName() << ", String=" << Ev1->get_StringArg() << endl;
+	cout << "Event Type: " << Ev2->get_EventType() << ", Client ID: " << Ev2->get_ClientID();
+	cout << ", Name: " << Ev2->get_PlayerName() << ", String=" << Ev2->get_StringArg() << endl;
+	cout << "Event Type: " << Ev3->get_EventType() << ", Client ID: " << Ev3->get_ClientID();
+	cout << ", Name: " << Ev3->get_PlayerName() << ", String=" << Ev3->get_StringArg() << endl;
+
+	//LogWatcher m_LogWatcher(L"C:\\Program Files (x86)\\Activision\\Call of Duty 4 - Modern Warfare\\Mods\\ModWarfare\\games_mp.log");
+
+	//Sleep(1000);
+
+	//for (int i=0; i<100; i++)
+	//{
+	//	if (m_LogWatcher.Update() > 0)
+	//		wcout << L"More Bytes" << endl;
+	//	Sleep(100);
+	//}
 
 	return 0;
 }
